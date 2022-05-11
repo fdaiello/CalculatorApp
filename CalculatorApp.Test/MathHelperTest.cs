d3fedc925889f9be82e9b34c323ac1bd01f0b633
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-05-11 19:56:40 -0300

    Theory, InLineData

--- CalculatorApp.Test/MathHelperTest.cs
+++ CalculatorApp.Test/MathHelperTest.cs
@@ -6,7 +6,7 @@ namespace CalculatorApp.Test
     public class MathHelperTest
     {
         [Fact]
-        public void TestIsEven ()
+        public void TestIsEven()
         {
             var cal = new MathFormulas();
 
@@ -19,6 +19,36 @@ namespace CalculatorApp.Test
             Assert.False(xResult);
             Assert.True(yResult);
 
+        }
+        [Theory]
+        [InlineData(2, 1, 1)]
+        [InlineData(10, 5, 5)]
+        public void TestDiff(int x, int y, int result)
+        {
+            var calculator = new MathFormulas();
+            Assert.Equal(result, calculator.Diff(y, x));
+        }
+        [Theory]
+        [InlineData(new int[] { 5, 5 }, 10)]
+        [InlineData(new int[] { 10, 10 }, 20)]
+        public void TestSum(int[] values, int result)
+        {
+            var calculator = new MathFormulas();
+            Assert.Equal(result, calculator.Sum(values));
+        }
+        [Theory(Skip ="not needed!")]
+        [InlineData(5, 5, 10)]
+        public void TestAdd(int x, int y, int result)
+        {
+            var calculator = new MathFormulas();
+            Assert.Equal(result, calculator.Add(x, y));
+        }
+        [Theory]
+        [InlineData(new int[] { 10, 20, 30 }, 20)]
+        public void TestAverage(int[] values, double result)
+        {
+            var calculator = new MathFormulas();
+            Assert.Equal(result, calculator.Average(values));
         }
     }
 }
